76b4c4e5dd16972c88581a23f5d46c35056e3313
Kasper Meerts <kaspermeerts@gmail.com>
2021-12-16 01:23:22 +0100

    Keep fish prompt closer to default

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,20 +5,12 @@ alias bc="bc -lq"
 alias gcc="gcc -Wall -Wextra"
 alias hd="hexdump -C"
 alias yolo="yaourt -Syua"
-alias temp="cat /sys/class/thermal/thermal_zone*/temp"
 alias ffmpeg="ffmpeg -hide_banner"
 
 set -e fish_greeting
 function fish_greeting -d "Greet the user"
-	git --git-dir=$HOME/.git --work-tree=$HOME diff-index --quiet HEAD --
+	git --git-dir=$HOME/.config/dotfiles --work-tree=$HOME diff-index --quiet HEAD --
 	or echo "dotfiles git repository needs updating"
-end
-
-# TODO __fish_git_prompt
-function fish_prompt -d "Write out the prompt"
-	and set -l pwd_color green
-	or  set -l pwd_color red
-	printf '%s%s%s> ' (set_color $pwd_color) (prompt_pwd) (set_color normal)
 end
 
 function humanize_duration
@@ -35,10 +27,10 @@ function humanize_duration
 	end
 end
 
-function fish_right_prompt
-	if not test $CMD_DURATION -a $CMD_DURATION -ge 500
-		return
-	end
-
-	set_color green; humanize_duration $CMD_DURATION; set_color normal;
-end
+#function fish_right_prompt
+#	if not test $CMD_DURATION -a $CMD_DURATION -ge 500
+#		return
+#	end
+#
+#	set_color green; humanize_duration $CMD_DURATION; set_color normal;
+#end
